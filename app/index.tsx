c49289f840a5d896b138b05d06f1350760fb24cd
ashish <ashish.v@foxberry.in>
2024-10-02 18:00:47 +0530

    added

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+// import { Link } from 'expo-router'
+// import React from 'react'
+// import { View,Text } from 'react-native'
+// // require("./about/about")
+// function index() {
+//   return (
+//     <View  style={{
+//       backgroundColor:"red",
+//       flex:1,
+//       justifyContent:"center",
+//       alignItems:"center"
+//     }}>
+     
+// <Text style={{fontSize:20,fontWeight:"700",color:"yellow"}}>Hello Welcome to EventAura World</Text>
+// <Link href={"./about/about"}>Go to about</Link>
+//     </View>
+//   )
+// }
+
+
+// export default index
